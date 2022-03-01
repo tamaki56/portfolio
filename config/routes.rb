c683fc5d34990c474411bca51d76077250608bff
EC2 Default User <ec2-user@ip-172-31-4-156.ec2.internal>
2022-03-01 13:07:42 +0900

    [Add]application.html/application_controller

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,9 @@ devise_for :admin, controllers: {
     root to: 'mikans#index'
     resources :mikans, only: [:show]
     get 'user/my_page' => 'users#show'
-    resources :users, only: [:edit, :update]
+    resources :users, only: [:edit, :update]do
+       get :favorites, on: :collection
+    end
     resources :recipes, only: [:index, :show, :new, :create, :edit, :update, :destroy]do
       resource :favorites, only: [:create, :destroy]
     end
@@ -25,7 +27,6 @@ devise_for :admin, controllers: {
   #管理者
   namespace :admin do
     resources :mikans
-    resources :customers, only: [:show,:index,:edit,:update]
     resources :tags, only: [:index,:edit,:create,:update, :destroy]
     resources :users, only: [:index,:show,:edit,:update, :destroy]
     resources :recipes, only: [:index, :show, :edit, :update, :destroy]
