c1973704a0919b4c3b51107044cf6b1359896d0f
EC2 Default User <ec2-user@ip-172-31-4-156.ec2.internal>
2022-03-07 13:26:58 +0900

    [Add]recipe/tag

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -3,12 +3,16 @@ class Admin::TagsController < ApplicationController
   def index
     @tags = Tag.all
   end
-
+  
   def destroy
     @tag = Tag.find(params[:id])  
     if @tag.destroy
       redirect_to admin_tags_path
     end
+  end
+  
+  def show
+    @tag = Tag.find(params[:id])
   end
   
   def tag_params
