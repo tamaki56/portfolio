1da0aca502a4a5254690078dba0fe465d91de06c
tamaki56 <http56@icloud.com>
2022-04-22 05:47:40 +0000

    add config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,6 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  
-  config.hosts << "5652a02aa2a7499aadc66aa8fe1f8ad0.vfs.cloud9.us-east-1.amazonaws.com"
+
+  config.hosts << "730c9cb05c8b4cdc91ca9f41e4581cac.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
