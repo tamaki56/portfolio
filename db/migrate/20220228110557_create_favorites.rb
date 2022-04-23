769c85ad0a1cb8516c7d6d81a4f5a6263d3acd04
tamaki56 <http56@icloud.com>
2022-04-23 06:54:11 +0000

    add

--- db/migrate/20220228110557_create_favorites.rb
+++ db/migrate/20220228110557_create_favorites.rb
@@ -1,9 +1,13 @@
 class CreateFavorites < ActiveRecord::Migration[6.1]
   def change
     create_table :favorites do |t|
-      t.references :user, type: :bigint, foreign_key: true
-      t.references :recipe, type: :bigint, foreign_key: true
+      t.bigint :user_id
+      t.bigint :recipe_id
+      # t.references :user, type: :bigint, foreign_key: true
+      # t.references :recipe, type: :bigint, foreign_key: true
       t.timestamps
     end
+    add_foreign_key :favorites, :users
+    add_foreign_key :favorites, :recipes
   end
 end
--- db/migrate/20220302112634_create_recipe_mikans.rb
+++ db/migrate/20220302112634_create_recipe_mikans.rb
@@ -2,7 +2,7 @@ class CreateRecipeMikans < ActiveRecord::Migration[6.1]
   def change
     create_table :recipe_mikans do |t|
       t.references :recipe, type: :bigint, foreign_key: true
-      t.references :mikan, type: :bigint, foreign_key: true
+      t.references :mikan,  foreign_key: true
       t.integer :amount, null: false
       
       t.timestamps
